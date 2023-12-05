0ac2f19a0c1dfe4d927df94dc3e9ec981d837d93
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2023-12-04 23:30:09 -0600

    Initial files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MiguelTornero/aoc-2023-day-1
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func isDigit(c rune) bool {
+	return c >= '0' && c <= '9'
+}
+
+func getFirstDigit(s string) (int, rune) {
+	for i, char := range s {
+		if isDigit(char) {
+			return i, char
+		}
+	}
+	return -1, 0
+}
+
+func getLastDigit(s string) (int, rune) {
+	outputIndex := -1
+	outputChar := rune(0)
+
+	for i, char := range s {
+		if isDigit(char) {
+			outputChar = char
+			outputIndex = i
+		}
+	}
+	return outputIndex, outputChar
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		i, char := getFirstDigit(line)
+
+		if i < 0 {
+			log.Fatalf("Invalid input found: %s", line)
+		}
+
+		firstDigit := char
+		secondDigit := firstDigit
+
+		i, char = getLastDigit(line[i:])
+		if i >= 0 {
+			secondDigit = char
+		}
+		fmt.Printf("%c%c\n", firstDigit, secondDigit)
+	}
+
+}
